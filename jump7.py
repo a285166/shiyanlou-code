844047fec02be5ae88758ca8203d9f7ac98d9d6e
a285166 <1930938140@qq.com>
2020-11-12 00:59:49 +0800

    shiyanlouban

---
+++ jump7.py
@@ -0,0 +1,13 @@
+list_number = []
+for i in range(100):
+    y = i + 1
+    if y % 7 == 0:
+        pass
+    elif y % 10 == 7:
+        pass
+    elif y % 10 == 7:
+        pass
+    else:
+        list_number.append(y)
+for i in list_number:
+    print(i)
